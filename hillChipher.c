4f40a6fb16eec9fb87eef9171bfc9f898cdcbeae
Nischay Gupta <79783629+NickDL29@users.noreply.github.com>
2023-04-17 11:49:23 +0530

    Update hillChipher.c

--- hillChipher.c
+++ hillChipher.c
@@ -1,44 +1,89 @@
 #include <stdio.h>
 #include <string.h>
+
+// Following function generates the
+// key matrix for the key string
+void getKeyMatrix(char* key, int keyMatrix[][3])
+{
+	int k = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			keyMatrix[i][j] = (key[k]) % 65;
+			k++;
+		}
+	}
+}
+
+// Following function encrypts the message
+void encrypt(int cipherMatrix[][1],
+			int keyMatrix[][3],
+			int messageVector[][1])
+{
+	int x, i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			cipherMatrix[i][j] = 0;
+		
+			for (x = 0; x < 3; x++)
+			{
+				cipherMatrix[i][j] +=
+					keyMatrix[i][x] * messageVector[x][j];
+			}
+		
+			cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
+		}
+	}
+}
+
+// Function to implement Hill Cipher
+void HillCipher(char* message, char* key)
+{
+	// Get key matrix from the key string
+	int keyMatrix[3][3];
+	getKeyMatrix(key, keyMatrix);
+
+	int messageVector[3][1];
+
+	// Generate vector for the message
+	for (int i = 0; i < 3; i++)
+		messageVector[i][0] = (message[i]) % 65;
+
+	int cipherMatrix[3][1];
+
+	// Following function generates
+	// the encrypted vector
+	encrypt(cipherMatrix, keyMatrix, messageVector);
+
+	char CipherText[4];
+	memset(CipherText, 0, sizeof(CipherText));
+
+	// Generate the encrypted text from
+	// the encrypted vector
+	for (int i = 0; i < 3; i++)
+		CipherText[i] = cipherMatrix[i][0] + 65;
+
+	// Finally print the ciphertext
+	printf("Ciphertext: %s\n", CipherText);
+}
+
+// Driver function for above code
 int main()
 {
-    unsigned int a[3][3] = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
-    unsigned int b[3][3] = {{8, 5, 10}, {21, 8, 21}, {21, 12, 8}};
-    int i, j;
-    unsigned int c[20], d[20];
-    char msg[20];
-    int determinant = 0, t = 0;
-    
-    printf("Enter plain text\n ");
-    scanf("%s", msg);
-    for (i = 0; i < 3; i++)
-    {
-        c[i] = msg[i] - 65;
-        printf("%d ", c[i]);
-    }
-    for (i = 0; i < 3; i++)
-    {
-        t = 0;
-        for (j = 0; j < 3; j++)
-        {
-            t = t + (a[i][j] * c[j]);
-        }
-        d[i] = t % 26;
-    }
-    printf("\nEncrypted Cipher Text :");
-    for (i = 0; i < 3; i++)
-        printf(" %c", d[i] + 65);
-    for (i = 0; i < 3; i++)
-    {
-        t = 0;
-        for (j = 0; j < 3; j++)
-        {
-            t = t + (b[i][j] * d[j]);
-        }
-        c[i] = t % 26;
-    }
-    printf("\nDecrypted Cipher Text :");
-    for (i = 0; i < 3; i++)
-        printf(" %c", c[i] + 65);
-    return 0;
+	// Get the message to be encrypted
+	char message[4];
+	printf("Enter a 3-letter message to encrypt: ");
+	scanf("%s", message);
+
+	// Get the key
+	char key[10];
+	printf("Enter a 9-letter key: ");
+	scanf("%s", key);
+
+	HillCipher(message, key);
+
+	return 0;
 }
