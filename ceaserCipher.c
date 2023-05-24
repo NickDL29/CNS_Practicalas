f33cbf35db2fa35e9e7dbf159eef22f2bf6fdb21
Nischay Gupta <79783629+NickDL29@users.noreply.github.com>
2023-05-24 22:48:25 +0530

    Updated ceaserCipher.c

--- ceaserCipher.c
+++ ceaserCipher.c
@@ -2,58 +2,32 @@
 
 #define MAX_LEN 100
 
-void encrypt(char message[], int shift);
-void decrypt(char message[], int shift);
+void ceaser(char message[], int shift) {
+
+  char ch;
+  for(int i = 0; message[i] ; i++) {
+      ch = (message[i] + shift - 'a') % 26 + 'a';
+      message[i] = ch;
+  }
+}
 
 int main() {
   char message[MAX_LEN];
   int shift;
-  char operation;
 
-  printf("Enter message: ");
-  fgets(message, MAX_LEN, stdin);
-
-  printf("Enter shift: ");
-  scanf("%d", &shift);
-
-  printf("Enter operation (e for encrypt, d for decrypt): ");
-  scanf(" %c", &operation);
-
-  switch(operation) {
-    case 'e':
-      encrypt(message, shift);
-      printf("Encrypted message: %s\n", message);
-      break;
-    case 'd':
-      decrypt(message, shift);
-      printf("Decrypted message: %s\n", message);
-      break;
-    default:
-      printf("Invalid operation\n");
-      break;
-  }
+    printf("Enter message: ");
+    scanf("%s", &message);
+    
+    printf("Enter shift: ");
+    scanf("%d", &shift);
+    
+    
+    ceaser(message, shift);
+    printf("\nEncrypted message: %s", message);
+    
+    ceaser(message, 26 - shift);
+    printf("\nDecrypted message: %s", message);
 
   return 0;
 }
 
-void encrypt(char message[], int shift) {
-  int i;
-  char ch;
-
-  for(i = 0; message[i] != '\0'; i++) {
-    ch = message[i];
-
-    if(ch >= 'a' && ch <= 'z') {
-      ch = (ch + shift - 'a') % 26 + 'a';
-    }
-    else if(ch >= 'A' && ch <= 'Z') {
-      ch = (ch + shift - 'A') % 26 + 'A';
-    }
-
-    message[i] = ch;
-  }
-}
-
-void decrypt(char message[], int shift) {
-  encrypt(message, 26 - shift);
-}
